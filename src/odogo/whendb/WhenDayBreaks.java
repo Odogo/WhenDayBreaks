4cd8b0cd5f65d22fd4730e4f229d7e0a256cbdb3
Odogo <77793565+Odogo@users.noreply.github.com>
2021-05-22 01:53:29 -0500

    Initial setup / Configuration
    - Basic setup
    - Configuration setup

---
+++ src/odogo/whendb/WhenDayBreaks.java
@@ -0,0 +1,18 @@
+package odogo.whendb;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WhenDayBreaks extends JavaPlugin {
+
+	@Override public void onLoad() {
+		super.onLoad();
+	}
+
+	@Override public void onEnable() {
+		super.onEnable();
+	}
+
+	@Override public void onDisable() {
+		super.onDisable();
+	}
+}
